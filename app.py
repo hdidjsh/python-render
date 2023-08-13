8ea1d2b741a076560725e205843d4d72a802dbd6
Daniel C <bitclaw@gmail.com>
2023-08-13 10:44:11 -0500

    flask-app-on-render
    * If we find a key called PROD_APP_SETTINGS in our environment, set the config for our app to be config.ProductionConfig , otherwise we can safely assume we are in development mode and set it to config.DevelopmentConfig

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
 
 
 @app.route("/")
